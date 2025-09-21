b32babf45c3a713bcfee2f6a4ff6a9e690570d52
Benji Smith <benji@shaxpir.com>
2025-09-21 10:52:59 -0700

    Export AttachedCollectionPerTableStrategy from package index
    The strategy class was defined but not exported from the main index.js file,
    causing a TypeError when trying to use it in applications.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Export Expo-specific adapters
 module.exports = {
   ExpoSqliteAdapter: require('./lib/adapters/expo-sqlite-adapter'),
-  AttachedExpoSqliteAdapter: require('./lib/adapters/attached-expo-sqlite-adapter')
+  AttachedExpoSqliteAdapter: require('./lib/adapters/attached-expo-sqlite-adapter'),
+  AttachedCollectionPerTableStrategy: require('./lib/schema/attached-collection-per-table-strategy')
 };
