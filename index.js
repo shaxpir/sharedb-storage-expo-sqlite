6e9b9197ae2dbe151ebb1fdc14884c76b8b6deeb
Benji Smith <benji@shaxpir.com>
2025-09-21 10:55:58 -0700

    Revert incorrect export - AttachedCollectionPerTableStrategy comes from base package

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Export Expo-specific adapters
 module.exports = {
   ExpoSqliteAdapter: require('./lib/adapters/expo-sqlite-adapter'),
-  AttachedExpoSqliteAdapter: require('./lib/adapters/attached-expo-sqlite-adapter'),
-  AttachedCollectionPerTableStrategy: require('./lib/schema/attached-collection-per-table-strategy')
+  AttachedExpoSqliteAdapter: require('./lib/adapters/attached-expo-sqlite-adapter')
 };
